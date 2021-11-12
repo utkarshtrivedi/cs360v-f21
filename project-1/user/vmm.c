008f00776b3316802bec9229227a438e25858fd7
Josh Pitts <joshua.d.pitts@gmail.com>
2021-11-12 12:46:27 -0600

    Adding in Instructor Solution
    Only merging the specific functions we were asked to edit.

--- project-1/user/vmm.c
+++ project-1/user/vmm.c
@@ -17,53 +17,51 @@
 //
 // Hint: Call sys_ept_map() for mapping page.
 static int
-map_in_guest(envid_t guest, uintptr_t gpa, size_t memsz,
-			 int fd, size_t filesz, off_t fileoffset)
-{
-	/* Your code here */
-	envid_t srcid = sys_getenvid();
-	int ret = 0;
-	int perm = PTE_P | PTE_U | PTE_W;
-	uintptr_t gpa_counter = gpa;
-    if(PGOFF(gpa_counter))  // Re-align the gpa to PAGE
-        ROUNDDOWN(gpa_counter, PGSIZE);
+map_in_guest( envid_t guest, uintptr_t gpa, size_t memsz,
+	      int fd, size_t filesz, off_t fileoffset ) {
+	int i, ret;
 
-	int i;
-    /**
-     * memsz is > filesz
-     * Round up next page size so the whole block is page aligned.
-     */
-	for (i = 0; i < ROUNDUP(memsz, PGSIZE); i += PGSIZE)
-	{
-        // Allocate the memory space
-		if ((ret = sys_page_alloc(srcid, UTEMP, perm)) < 0)
-			return ret;
-
-        // Still in the file:
-        if(i < filesz) {
-            // Seek in the file:
-            if( (ret=seek(fd, fileoffset+i)) < 0)
-                return ret;
-            // How much to load from the file:
-            size_t amount_to_load = filesz - i;
-            if(PGSIZE < amount_to_load)  // If we are at the end of the file but less than page size to laod...
-                amount_to_load = PGSIZE;
-            // Now load:
-            if( (ret=readn(fd, UTEMP, amount_to_load)) < 0)
-                return ret;
-        }
-        // Do Mapping:
-        if ((ret = sys_ept_map(srcid, UTEMP,
-                               guest, (void *)gpa_counter+i,
-                               perm)) < 0)
-            return ret;
-
-        // Clear mem, release
-        if ((ret = sys_page_unmap(srcid, UTEMP)) < 0)
-            return ret;
+	i = PGOFF(gpa);
+	// if the provided guest physical address is not page-aligned,
+	// adjust our values so we are working with a page-aligned address
+	if (i) {
+		gpa -= i;
+		memsz += i;
+		filesz += i;
+		fileoffset -= i;
 	}
 
-	return ret;
+	// walk through the provided region page by page and copy in the file contents
+	// the the physical memory region of the guest
+	for (i = 0; i < memsz; i += PGSIZE) {
+		// allocate a temporary page
+		ret = sys_page_alloc(0, UTEMP, PTE_P | PTE_U | PTE_W);
+		if (ret < 0) {
+			sys_page_unmap(0, UTEMP);
+			return ret;
+		}
+		// seek to the location to write the file contents at
+		ret = seek(fd, fileoffset + i);
+		if (ret < 0) {
+			sys_page_unmap(0, UTEMP);
+			return ret;
+		}
+		// read file contents into the mapped page
+		ret = readn(fd, UTEMP, MIN(PGSIZE, filesz-i));
+		if (ret < 0) {
+			sys_page_unmap(0, UTEMP);
+			return ret;
+		}
+		// map the page in the EPT
+		ret = sys_ept_map(0, UTEMP, guest, (void*) (gpa + i), __EPTE_FULL);
+		if (ret < 0) {
+			sys_page_unmap(0, UTEMP);
+			return ret;
+		}
+		sys_page_unmap(0, UTEMP);
+	}
+
+	return 0;
 }
 
 // Read the ELF headers of kernel file specified by fname,
@@ -73,44 +71,52 @@ map_in_guest(envid_t guest, uintptr_t gpa, size_t memsz,
 //
 // Hint: compare with ELF parsing in env.c, and use map_in_guest for each segment.
 static int
-copy_guest_kern_gpa(envid_t guest, char *fname)
-{
-	/* Your code here */
-	int fd;
- 	if ((fd = open(fname, O_RDONLY)) < 0)
-        return -E_NOT_FOUND;  // Return File not found error or can't open
-
-	struct Elf *elf;
-	unsigned char elf_buf[512];
-
-	// Read elf header
-	elf = (struct Elf *)elf_buf;
-	if (readn(fd, elf_buf, sizeof(elf_buf)) != sizeof(elf_buf) || elf->e_magic != ELF_MAGIC)
-	{
-		close(fd);
-		cprintf("elf magic %08x want %08x\n", elf->e_magic, ELF_MAGIC);
-		return -E_NOT_EXEC;  // File not a valid executable
+copy_guest_kern_gpa( envid_t guest, char* fname ) {
+	// open the specified file
+	int fd, i, ret;
+	fd = open(fname, O_RDONLY);
+	if (fd < 0) {
+		cprintf("error opening %s: %e\n", fname, fd);
+		return fd;
 	}
 
-	struct Proghdr *ph, *eph;
+	int buflen = 512; // large enough to fit the whole Elf struct
+	unsigned char elf_buf[buflen];
+	struct Elf *elf;
+	struct Proghdr *ph;
 
-	if (elf && elf->e_magic == ELF_MAGIC)  // Extra check
-	{
-		ph = (struct Proghdr *)((uint8_t *)elf + elf->e_phoff);
-		eph = ph + elf->e_phnum;
-		for (; ph < eph; ph++)
-		{
-			if (ph->p_type == ELF_PROG_LOAD)
-			{
-				if (map_in_guest(guest, ph->p_pa, ph->p_memsz, fd, ph->p_filesz, ph->p_offset) < 0)
-				{
-					close(fd);
-					return -E_BAD_ENV;  // Environment Error, cannot load Kernel
-				}
-			}
+	// read in the header of file fname
+	ret = readn(fd, elf_buf, buflen);
+	if (ret != buflen) {
+		close(fd);
+		cprintf("Failed to read in ELF header\n");
+		return -E_NOT_EXEC;
+	}
+
+	// point the elf struct at the elf buf
+	elf = (struct Elf*) elf_buf;
+
+	// check the ELF header's magic value to check that we are working with a valid ELF binary
+	if (elf->e_magic != ELF_MAGIC) {
+		close(fd);
+		cprintf("ELF magic is %08x, expected %08x\n", elf->e_magic, ELF_MAGIC);
+	}
+
+	// point the program header struct at the location indicated by the elf header
+	ph = (struct Proghdr* ) (elf_buf + elf->e_phoff);
+
+	// for every entry in the program header table, map the corresponding entry of the file
+	// into the guest's physical address space
+	for (i = 0; i < elf->e_phnum; i++, ph++) {
+		ret = map_in_guest(guest, ph->p_pa, ph->p_memsz, fd, ph->p_filesz, ph->p_offset);
+		if (ret < 0) {
+			cprintf("Error mapping EPT page in guest %e\n", ret);
+			close(fd);
+			return ret;
 		}
 	}
 
+	close(fd);
 	return 0;
 }
 
